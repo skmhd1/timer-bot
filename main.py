5946f3fc7178dbe864d1ae61d7bfd8019ad4ebc0
skmhd1 <skabdullahmahdi@gmail.com>
2025-04-26 13:31:27 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord, os
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(os.getenv("TOKEN"))
