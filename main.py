2c3163c39632c24bdec95db703de9f60e5e66817
skmhd1 <skabdullahmahdi@gmail.com>
2025-04-26 14:14:42 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import discord, os
+import discord
+import os
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
